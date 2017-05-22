d351a5a8d57d6853930f025e07708d8af841f864
leeboy0333 <leeboy_0333@163.com>
2017-05-22 22:07:29 +0800

    binary seperate method update

--- BinaryRootAlgorighom/src/implement/BinaryRoute.java
+++ BinaryRootAlgorighom/src/implement/BinaryRoute.java
@@ -10,11 +10,8 @@ import java.util.*;
  */
 public class BinaryRoute {
     private Map<Point, Set<Line>> point2Lines =null;
-    private List<Line> route(List<Line> lines, Point pIn, Point pOut, boolean init)
+    public List<Line> route(List<Line> lines, Point pIn, Point pOut)
     {
-        if(init){
-            initPoint2Lines(lines);
-        }
         if(pIn.equals(pOut))
         {
             return new ArrayList<>(0);
@@ -47,17 +44,17 @@ public class BinaryRoute {
         {
             if(linesA.isEmpty())
             {
-                return route(linesB, pIn, pOut, false);
+                return route(linesB, pIn, pOut);
             }
             else
             {
-                return route(linesA, pIn, pOut,false);
+                return route(linesA, pIn, pOut);
             }
         }
         for(Point pointAB : pointsAB)
         {
-            List<Line> linesInA = route(linesA, pIn, pointAB,false);
-            List<Line> linesInB = route(linesB, pointAB, pOut,false);
+            List<Line> linesInA = route(linesA, pIn, pointAB);
+            List<Line> linesInB = route(linesB, pointAB, pOut);
             if(linesInA == null || linesInB == null)
             {
                 continue;
@@ -72,14 +69,6 @@ public class BinaryRoute {
         return minRouteLines;
     }
 
-    private void initPoint2Lines(List<Line> lines) {
-        for(Line line :lines)
-        {
-            mapAppend(point2Lines, line.getP1(), line);
-            mapAppend(point2Lines, line.getP2(), line);
-        }
-    }
-
     private <K,V> void mapAppend(Map<K, Set<V>> mapSet, K key, V val)
     {
         Set<V> valSet = mapSet.get(key);
@@ -91,21 +80,71 @@ public class BinaryRoute {
         valSet.add(val);
     }
 
-    public List<Line> route(List<Line> lines, Point pIn, Point pOut)
-    {
-        return route(lines, pIn, pOut, true);
-    }
-
     private int compareRoute(List<Line> minRouteLines, List<Line> result) {
         return minRouteLines.size() - result.size();
     }
 
     private void binarySeperateLines(List<Line> lines, List<Line> linesA, List<Line> linesB, Point pointA, Point pointB, Set<Point> pointsAB)
     {
-        boolean toA = true;
+        final int halfLineSize = lines.size() >> 1;
+        Set<Line> linesToAdd = new HashSet<>();
+        Map<Point, Set<Line>> point2Line = piont2LineSetMap(lines);
+        Set<Point> nextPoints = new HashSet<>(Arrays.asList(pointA));
+
+        addLineToA:
+        while(!nextPoints.isEmpty())
+        {
+            Set<Point> nextPointsTmp = new HashSet<>();
+            for(Point point : nextPoints)
+            {
+                Set<Line> nextLines = point2Line.remove(point);
+                if(nextLines == null)
+                {
+                    continue;
+                }
+                for(Line line : nextLines)
+                {
+                    if(linesToAdd.contains(line) || line.getP1().equals(pointB) || line.getP2().equals(pointB))
+                    {
+                        continue;
+                    }
+                    linesToAdd.add(line);
+                    if(linesToAdd.size() >= halfLineSize)
+                    {
+                        break addLineToA;
+                    }
+                    if(line.getP1().equals(point))
+                    {
+                        nextPointsTmp.add(line.getP2());
+                    }
+                    else if(line.getP2().equals(point))
+                    {
+                        nextPointsTmp.add(line.getP1());
+                    }
+                }
+            }
+            nextPoints = nextPointsTmp;
+        }
+
         Set<Point> pointsA = new HashSet<>();
         Set<Point> pointsB = new HashSet<>();
-         
+
+        for(Line line : lines)
+        {
+            if(linesToAdd.contains(line))
+            {
+                linesA.add(line);
+                pointsA.add(line.getP1());
+                pointsA.add(line.getP2());
+            }
+            else
+            {
+                linesB.add(line);
+                pointsB.add(line.getP1());
+                pointsB.add(line.getP2());
+            }
+        }
+
         for(Point point : pointsA)
         {
             if(pointsB.contains(point))
@@ -114,6 +153,17 @@ public class BinaryRoute {
             }
         }
     }
+
+    private Map<Point, Set<Line>> piont2LineSetMap(List<Line> lines) {
+        Map<Point, Set<Line>> point2LineSetMap = new HashMap<>();
+        for(Line line : lines)
+        {
+            mapAppend(point2LineSetMap, line.getP1(), line);
+            mapAppend(point2LineSetMap, line.getP2(), line);
+        }
+        return point2LineSetMap;
+    }
+
 
     private boolean isLinesContainsPoint(List<Line> lines,  Point point)
     {
